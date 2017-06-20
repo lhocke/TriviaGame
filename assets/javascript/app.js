b4f822f906e31edac387fa80f7b8f526575b0a42
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-06-20 16:42:00 -0700

    updating js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,11 @@
 var triviaQuestions = {
 	"Where is the Venus de Milo Currently displayed?" : ["The Louvre", "The British Museum", "Uffizi", "The Vatican"],
 	"Which of the following was painted by Vincent Van Gogh?" : ["Starry Night", "Water Lillies", "The Persistance of Memory", "On The River Blue"],
-	"Who painted the Mona Lisa" : ["", "", "", ""],
-	
-
+	"Who painted the Mona Lisa?" : ["Leonardo da Vinci", "Pablo Picasso", "Caravaggio", "Raphael"],
+	"Who is the creator of the statue of David?" : ["Michaelangelo", "Leornardo da Vinci", "Donatello", "Raphael"],
+	"What famous event from the Bible did Leonardo da Vinci paint?" : ["The Crucifixion", "Noah's Ark", "David and Goliath", "The Last Supper"]
 }
 
+function game(){
+	
+}
