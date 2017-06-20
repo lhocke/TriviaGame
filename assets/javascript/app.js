e35be5f2fb45f005948aea1806cabe0cf07fc6c3
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-06-20 16:17:11 -0700

    adding beginnings of js

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+var triviaQuestions = {
+	"Where is the Venus de Milo Currently displayed?" : ["The Louvre", "The British Museum", "Uffizi", "The Vatican"],
+	"Which of the following was painted by Vincent Van Gogh?" : ["Starry Night", "Water Lillies", "The Persistance of Memory", "On The River Blue"],
+	"Who painted the Mona Lisa" : ["", "", "", ""],
+	
+
+}
+
