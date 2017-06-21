d461ccc5f78b86f68076739a9c09007a139bb72f
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-06-21 13:20:16 -0700

    attempting question object clean

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,18 @@
 var triviaQuestions = {
-	"Where is the Venus de Milo Currently displayed?" : ["The Louvre", "The British Museum", "Uffizi", "The Vatican"],
-	"Which of the following was painted by Vincent Van Gogh?" : ["Starry Night", "Water Lillies", "The Persistance of Memory", "On The River Blue"],
-	"Who painted the Mona Lisa?" : ["Leonardo da Vinci", "Pablo Picasso", "Caravaggio", "Raphael"],
-	"Who is the creator of the statue of David?" : ["Michaelangelo", "Leornardo da Vinci", "Donatello", "Raphael"],
-	"What famous event from the Bible did Leonardo da Vinci paint?" : ["The Crucifixion", "Noah's Ark", "David and Goliath", "The Last Supper"]
+	q1: {
+		question:"Where is the Venus de Milo Currently displayed?",
+		correct:"The Louvre",
+		answers: [{
+			possible1: "The Louvre", 
+			possible2: "The British Museum",
+			possible3: "Uffizi",
+			possible4: "The Vatican"}],
+	}
+	//  ["The Louvre", "The British Museum", "Uffizi", "The Vatican"],
+	// "Which of the following was painted by Vincent Van Gogh?" : ["Starry Night", "Water Lillies", "The Persistance of Memory", "On The River Blue"],
+	// "Who painted the Mona Lisa?" : ["Leonardo da Vinci", "Pablo Picasso", "Caravaggio", "Raphael"],
+	// "Who is the creator of the statue of David?" : ["Michaelangelo", "Leornardo da Vinci", "Donatello", "Raphael"],
+	// "What famous event from the Bible did Leonardo da Vinci paint?" : ["The Crucifixion", "Noah's Ark", "David and Goliath", "The Last Supper"]
 }
 
 function game(){
