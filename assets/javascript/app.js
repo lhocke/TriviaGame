aabfe1bfc885a5d21896174d17723e0e3a812b5b
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-06-22 09:58:15 -0700

    building out question base

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,36 @@
 var triviaQuestions = {
 	q1: {
-		question:"Where is the Venus de Milo Currently displayed?",
-		correct:"The Louvre",
-		answers: [{
-			possible1: "The Louvre", 
-			possible2: "The British Museum",
-			possible3: "Uffizi",
-			possible4: "The Vatican"}],
+		question : "Where is the Venus de Milo Currently displayed?",
+		correct : "The Louvre",
+		answers : ["The Louvre","The British Museum", "Uffizi", "The Vatican"],
+	},
+	q2: {
+		question : "Which of the following was painted by Vincent Van Gogh?",
+		correct : "Starry Night",
+		answers : ["Starry Night", "Water Lillies", "The Persistance of Memory", "On the River Blue"]
+	},
+	q3: {
+		question : "Who painted the Mona Lisa?",
+		correct : "Leonardo da Vinci",
+		answers: ["Leonardo da Vinci", "Pablo Picasso", "Caravaggio", "Raphael"],
+	},
+	q4: {
+		question : "Who is the creator of the statue of David?",
+		correct : "Michaelangelo",
+		answers : ["Michaelangelo", "Leornardo da Vinci", "Donatello", "Raphael"],
+	},
+	q5: {
+		question: "What famous event from the Bible did Leonardo da Vinci paint?",
+		correct: "The Last Supper",
+		answers : ["The Crucifixion", "Noah's Ark", "David and Goliath", "The Last Supper"]
+	},
+	q6: {
+		question : "Which of the following Egyptian historical periods were responsible for "
 	}
-	//  ["The Louvre", "The British Museum", "Uffizi", "The Vatican"],
-	// "Which of the following was painted by Vincent Van Gogh?" : ["Starry Night", "Water Lillies", "The Persistance of Memory", "On The River Blue"],
-	// "Who painted the Mona Lisa?" : ["Leonardo da Vinci", "Pablo Picasso", "Caravaggio", "Raphael"],
-	// "Who is the creator of the statue of David?" : ["Michaelangelo", "Leornardo da Vinci", "Donatello", "Raphael"],
-	// "What famous event from the Bible did Leonardo da Vinci paint?" : ["The Crucifixion", "Noah's Ark", "David and Goliath", "The Last Supper"]
+
+
 }
 
-function game(){
+// function game(){
 	
-}
+// }
